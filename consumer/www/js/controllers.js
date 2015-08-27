c60fe199b18dc052f5c1ba5a3d4be5d6d4a49d11
Chris Wong <chris@cogifire.com>
2015-08-27 20:40:52 +0800

    Push JS

--- consumer/www/js/controllers.js
+++ consumer/www/js/controllers.js
@@ -2,6 +2,32 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
+.controller('DashCtrl', function($scope, $rootScope, $ionicUser, $ionicPush) {
+  
+  // Identifies a user with the Ionic User service
+  $scope.identifyUser = function() {
+    // Your identify code from before
+  };
+  
+  // Registers a device for push notifications and stores its token
+  $scope.pushRegister = function() {
+    console.log('Ionic Push: Registering user');
+
+    // Register with the Ionic Push service.  All parameters are optional.
+    $ionicPush.register({
+      canShowAlert: true, //Can pushes show an alert on your screen?
+      canSetBadge: true, //Can pushes update app icon badges?
+      canPlaySound: true, //Can notifications play a sound?
+      canRunActionsOnWake: true, //Can run actions outside the app,
+      onNotification: function(notification) {
+        // Handle new push notifications here
+        // console.log(notification);
+        return true;
+      }
+    });
+  };
+})
+
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
