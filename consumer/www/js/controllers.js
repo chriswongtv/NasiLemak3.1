a61ee6cb7e19f174b5b3dadedfbe069d125d8078
Chris Wong <chris@cogifire.com>
2015-08-28 03:34:33 +0800

    Rewards UI

--- consumer/www/js/controllers.js
+++ consumer/www/js/controllers.js
@@ -52,5 +52,31 @@ angular.module('starter.controllers', [])
   ];
 })
 
+app.controller('MainCtrl', function($scope, $ionicModal) {
+  $scope.contact = {
+    name: 'Mittens Cat',
+    info: 'Tap anywhere on the card to open the modal'
+  }
+
+  $ionicModal.fromTemplateUrl('rewards.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal
+  })  
+
+  $scope.openModal = function() {
+    $scope.modal.show()
+  }
+
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+})
+
 .controller('PlaylistCtrl', function($scope, $stateParams) {
 });
