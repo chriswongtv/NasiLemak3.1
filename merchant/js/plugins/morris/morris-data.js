bd326be865ebe10cd5854be861973098e9d1d89f
EshaantheMelon <eshaanmenon@gmail.com>
2015-08-28 03:09:36 +0800

    Merchant Panel at 75% completion
    At this point we' might be able to implement those features we ditched
    after all.  Fingers Crossed!

--- merchant/js/plugins/morris/morris-data.js
+++ merchant/js/plugins/morris/morris-data.js
@@ -68,14 +68,17 @@ $(function() {
     Morris.Donut({
         element: 'morris-donut-chart',
         data: [{
-            label: "Download Sales",
-            value: 12
+            label: "13-20 years old",
+            value: 15
         }, {
-            label: "In-Store Sales",
-            value: 30
+            label: "21-40 years old",
+            value: 35
         }, {
-            label: "Mail-Order Sales",
-            value: 20
+            label: "41-60 years old",
+            value: 35
+		}, {
+			label: "61-80 years old",
+			value: 15
         }],
         resize: true
     });
@@ -87,98 +90,29 @@ $(function() {
         // Chart data records -- each entry in this array corresponds to a point on
         // the chart.
         data: [{
-            d: '2012-10-01',
-            visits: 802
+            d: '1) 12:00am -2:59am',
+            visits: 7
         }, {
-            d: '2012-10-02',
-            visits: 783
+            d: '2) 3:00am- 5:59am',
+            visits: 12
         }, {
-            d: '2012-10-03',
-            visits: 820
+            d: '3) 6:00am-8:59am',
+            visits: 14
         }, {
-            d: '2012-10-04',
-            visits: 839
+            d: '4) 9:00am-11:59pm',
+            visits: 16
         }, {
-            d: '2012-10-05',
-            visits: 792
+            d: '5) 12:00pm-2:59pm',
+            visits: 18
         }, {
-            d: '2012-10-06',
-            visits: 859
+            d: '6) 3:00pm-5:59pm',
+            visits: 20
         }, {
-            d: '2012-10-07',
-            visits: 790
+            d: '7) 6:00pm-8:59pm',
+            visits: 18
         }, {
-            d: '2012-10-08',
-            visits: 1680
-        }, {
-            d: '2012-10-09',
-            visits: 1592
-        }, {
-            d: '2012-10-10',
-            visits: 1420
-        }, {
-            d: '2012-10-11',
-            visits: 882
-        }, {
-            d: '2012-10-12',
-            visits: 889
-        }, {
-            d: '2012-10-13',
-            visits: 819
-        }, {
-            d: '2012-10-14',
-            visits: 849
-        }, {
-            d: '2012-10-15',
-            visits: 870
-        }, {
-            d: '2012-10-16',
-            visits: 1063
-        }, {
-            d: '2012-10-17',
-            visits: 1192
-        }, {
-            d: '2012-10-18',
-            visits: 1224
-        }, {
-            d: '2012-10-19',
-            visits: 1329
-        }, {
-            d: '2012-10-20',
-            visits: 1329
-        }, {
-            d: '2012-10-21',
-            visits: 1239
-        }, {
-            d: '2012-10-22',
-            visits: 1190
-        }, {
-            d: '2012-10-23',
-            visits: 1312
-        }, {
-            d: '2012-10-24',
-            visits: 1293
-        }, {
-            d: '2012-10-25',
-            visits: 1283
-        }, {
-            d: '2012-10-26',
-            visits: 1248
-        }, {
-            d: '2012-10-27',
-            visits: 1323
-        }, {
-            d: '2012-10-28',
-            visits: 1390
-        }, {
-            d: '2012-10-29',
-            visits: 1420
-        }, {
-            d: '2012-10-30',
-            visits: 1529
-        }, {
-            d: '2012-10-31',
-            visits: 1892
+            d: '8) 9:00pm-11:59pm',
+            visits: 17
         }, ],
         // The name of the data record attribute that contains x-visitss.
         xkey: 'd',
@@ -196,27 +130,24 @@ $(function() {
     Morris.Bar({
         element: 'morris-bar-chart',
         data: [{
-            device: 'iPhone',
-            geekbench: 136
+            device: 'Chocolate',
+            geekbench: 7
         }, {
-            device: 'iPhone 3G',
-            geekbench: 137
+            device: 'Ice Cream',
+            geekbench: 8
         }, {
-            device: 'iPhone 3GS',
-            geekbench: 275
+            device: 'Hair Spray',
+            geekbench: 2
         }, {
-            device: 'iPhone 4',
-            geekbench: 380
+            device: 'Keychain',
+            geekbench: 15
         }, {
-            device: 'iPhone 4S',
-            geekbench: 655
-        }, {
-            device: 'iPhone 5',
-            geekbench: 1571
+            device: 'RC Helicopter',
+            geekbench: 1
         }],
         xkey: 'device',
         ykeys: ['geekbench'],
-        labels: ['Geekbench'],
+        labels: ['Redemptions'],
         barRatio: 0.4,
         xLabelAngle: 35,
         hideHover: 'auto',
