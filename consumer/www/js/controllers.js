d903e5980be1549cc7b900d52b1b03873f5c2e15
Chris Wong <chris@cogifire.com>
2015-08-28 12:01:09 +0800

    Ask for help

--- consumer/www/js/controllers.js
+++ consumer/www/js/controllers.js
@@ -54,6 +54,54 @@ var ctx = document.getElementsByTagName('canvas')[0].getContext('2d');
 ctx.lineWidth = width;
 ctx.strokeStyle = 'rgba(39, 174, 96, 1)';
 
+// The watch id references the current `watchAcceleration`
+    var watchID = null;
+
+    // Wait for Cordova to load
+    //
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+    // Cordova is ready
+    //
+    function onDeviceReady() {
+        startWatch();
+    }
+
+    // Start watching the acceleration
+    //
+    function startWatch() {
+
+        // Update acceleration every 3 seconds
+        var options = { frequency: 3000 };
+
+        watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+    }
+
+    // Stop watching the acceleration
+    //
+    function stopWatch() {
+        if (watchID) {
+            navigator.accelerometer.clearWatch(watchID);
+            watchID = null;
+        }
+    }
+
+    // onSuccess: Get a snapshot of the current acceleration
+    //
+    function onSuccess(acceleration) {
+        var element = document.getElementById('accelerometer');
+        element.innerHTML = 'Acceleration X: ' + acceleration.x + '<br />' +
+                            'Acceleration Y: ' + acceleration.y + '<br />' +
+                            'Acceleration Z: ' + acceleration.z + '<br />' +
+                            'Timestamp: '      + acceleration.timestamp + '<br />';
+    }
+
+    // onError: Failed to get the acceleration
+    //
+    function onError() {
+        alert('onError!');
+    }
+
 for (i = start; i < mid; i++) {
     var drawLeft = window.setTimeout(function () {
         ctx.beginPath();
